5c937b7835fe6d34bf378d5123a96d79802d425f
Rajit Dang <r3dang@ucsd.edu>
2017-02-28 02:09:03 -0800

    Tired af

--- twitter_client_codepath/twitter_client_codepath/User.swift
+++ twitter_client_codepath/twitter_client_codepath/User.swift
@@ -14,7 +14,10 @@ class User: NSObject {
     var profileURL: NSURL?
     var tagline: NSString?
     
+    var dictionary: NSDictionary?
+    
     init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
         name = dictionary["name"] as? String as NSString?
         screenname = dictionary["screen_name"] as? String as NSString?
         let profileURLString = dictionary["profile_image_url_https"] as? String
@@ -28,7 +31,24 @@ class User: NSObject {
     
     class var currentUser: User? {
         get {
-            return nil
+            let defaults = UserDefaults.standard
+            let userData = defaults.object(forKey: "currentUserData") as! NSData
+            if let userData = userData {
+                let dictionary = try! JSONSerialization.jsonObject(with: userData, options: [])
+            }
+        }
+        
+        set(user) {
+            let defaults = UserDefaults.standard
+            
+            if let user = user {
+                let data = try! JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+                defaults.set(data, forKey: "currentUserData")
+            } else {
+                defaults.set(nil, forKey: "currentUserData")
+            }
+            
+            defaults.synchronize()
         }
     }
     
